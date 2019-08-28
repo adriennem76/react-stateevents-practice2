bd5798f27a91a6e63fa555772a345edeae336e75
adriennem76 <adrienne.c.miranda@gmail.com>
2019-08-28 16:22:51 -0400

    patch added

--- src/App.js
+++ src/App.js
@@ -10,13 +10,23 @@ class App extends Component {
 
   handleClick = (bey) => {
     bey.favorite = !bey.favorite
-    bey.favorite ? this.setState({favoriteBeys: [...this.state.favoriteBeys, bey]}) : this.setState({favoriteBeys: this.state.favoriteBeys.filter(eachBey => eachBey != bey)}) 
+    bey.favorite ? this.setState({favoriteBeys: [...this.state.favoriteBeys, bey]}) : this.setState({favoriteBeys: this.state.favoriteBeys.filter(eachBey => eachBey != bey)})
+    fetch(`http://localhost:4000/beys/${bey.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({favorite: bey.favorite}) 
+    })
   }
 
   componentDidMount(){
     fetch("http://localhost:4000/beys")
     .then(resp => resp.json())
-    .then(data => this.setState({beys: data}))
+    .then(data => {
+      this.setState({beys: data})
+      this.setState({favoriteBeys: data.filter(bey => bey.favorite)})
+    })
   }
 
   render(){
